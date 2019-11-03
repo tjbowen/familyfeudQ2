5777429cb6bea68574e62a52ee063190a4a7fa0b
Tristan Bowen <tjb2317@gmail.com>
2019-11-03 15:58:54 -0600

    added q&a's, started scoreboard

--- js/all_questions.js
+++ js/all_questions.js
@@ -1,14 +1,64 @@
 // This is the file that contains all the question, answer and point value data.
 
 var json = [{
-	// Example Object
-	"question": "Question 1 goes here",
-	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+	// Game 1 Question 1
+	"question": "Who is Will Rodgers?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
 	"points": [21, 17, 16, 16, 8, 8, 7, 7]
 },{
-	// Example Object
-	"question": "Question 2 goes here",
-	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+	// Game 1 Question 2
+	"question": "In 3 words or less, how does the internet work?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 1 Question 3
+	"question": "What do the letters CMS stand for?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 1 Question 4
+	"question": "If Q2 had a mascot, what would it be?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 2 Question 1
+	"question": "Other than Will Rodgers, name someone on the Creative Services team.",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 2 Question 2
+	"question": "How did you find out that Santa is not real?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 2 Question 3
+	"question": "What does Creative Services do?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 2 Question 4
+	"question": "You have to replace your hands, other than feet what do you have for hands?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 3 Question 1
+	"question": "What is the best feature you have seen on a website?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 3 Question 2
+	"question": "What is Hank's spirit animal?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 3 Question 3
+	"question": "What does Creative Services do?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},{
+	// Game 3 Question 4
+	"question": "You're about to get into a fight, what song comes on as your soundtrack?",
+	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5", "Answer 6", "Answer 7", "Answer 8"],
 	"points": [21, 17, 16, 16, 8, 8, 7, 7]
 }
 	//...Append additional questions here
